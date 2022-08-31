6c2d541cf6c21c2c15bd997a670b93b59c01be94
Subhadeep Banerjee <subhadeep762@gmail.com>
2022-08-31 05:31:23 +0530

    Add terraform code to make ec2 with ebs volume and key-pair

---
+++ ec2/main.tf
@@ -0,0 +1,35 @@
+resource "aws_key_pair" "ec2_deploy_key" {
+  key_name   = "ec2_instance"
+  public_key = var.ec2_key_pair_public_key
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami = var.ec2_ami
+
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.ec2_deploy_key.key_name
+
+  monitoring           = var.monitoring
+  iam_instance_profile = var.instance_profile
+
+  associate_public_ip_address = var.public_ip_association
+  ebs_optimized               = var.ebs_optimized
+
+  user_data = file("./data/install.sh")
+
+  tags = {
+    Name    = var.instance_name
+    Billing = var.instance_name
+  }
+
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = var.delete_on_termination
+    volume_size           = var.volume_size
+    volume_type           = var.volume_type
+
+    tags = {
+      Name = var.instance_name
+    }
+  }
+}
