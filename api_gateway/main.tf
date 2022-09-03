b466d2b296061330c4d8cc4afef879bd9f18f001
Subhadeep Banerjee <subhadeep762@gmail.com>
2022-09-04 03:48:16 +0530

    Change method from {+proxy} to get, Reorder code, remvoe unnecessary depends_on.

--- api_gateway/main.tf
+++ api_gateway/main.tf
@@ -30,27 +30,23 @@ resource "aws_api_gateway_resource" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = "{proxy+}"
-
-  depends_on = [
-    aws_api_gateway_rest_api.this
-  ]
-
 }
 
 resource "aws_api_gateway_method" "this" {
-  rest_api_id      = aws_api_gateway_rest_api.this.id
-  resource_id      = aws_api_gateway_resource.this.id
-  http_method      = "ANY"
-  authorization    = "NONE"
-  api_key_required = true
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.this.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
 
-  request_parameters = {
-    "method.request.path.proxy" = true
-  }
+resource "aws_api_gateway_integration" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.this.id
+  http_method = aws_api_gateway_method.this.http_method
 
-  depends_on = [
-    aws_api_gateway_resource.this
-  ]
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.lambda.invoke_arn
 }
 
 resource "aws_api_gateway_method_response" "this_200" {
@@ -64,16 +60,6 @@ resource "aws_api_gateway_method_response" "this_200" {
   ]
 }
 
-resource "aws_api_gateway_integration" "this" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  resource_id = aws_api_gateway_resource.this.id
-  http_method = aws_api_gateway_method.this.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = module.lambda.invoke_arn
-}
-
 resource "aws_api_gateway_integration_response" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   resource_id = aws_api_gateway_resource.this.id
@@ -85,24 +71,25 @@ resource "aws_api_gateway_integration_response" "this" {
   ]
 }
 
-resource "aws_lambda_permission" "lambda_permission" {
+resource "aws_lambda_permission" "apigw_lambda_perm" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = module.lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.this.id}/*/${aws_api_gateway_method.this.http_method}${aws_api_gateway_resource.this.path}"
 
   depends_on = [
-    module.lambda
+    aws_api_gateway_rest_api.this
   ]
 }
 
 resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   description = "Deployment of AWS API Gateway HTTP Rest API"
-  triggers = {
 
+  triggers = {
     redeployment = sha1(jsonencode([
       aws_api_gateway_resource.this.id,
       aws_api_gateway_method.this.id,
