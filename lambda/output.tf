ddac1229b1281d64247d503b91e42d2fa84b93b8
Subhadeep Banerjee <subhadeep762@gmail.com>
2022-09-05 06:36:46 +0530

    Add function arn to lambda output

--- lambda/output.tf
+++ lambda/output.tf
@@ -10,6 +10,10 @@ output "invoke_arn" {
   value = aws_lambda_function.lambda.invoke_arn
 }
 
+output "function_arn" {
+  value = aws_lambda_function.lambda.arn
+}
+
 output "function_name" {
   value = aws_lambda_function.lambda.function_name
 }
