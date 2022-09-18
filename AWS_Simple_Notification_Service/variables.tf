fb9164a4a46cccf591de2716ba1f1125653b403f
Subhadeep Banerjee <subhadeep762@gmail.com>
2022-09-18 19:38:27 +0530

    Add variables for SNS

--- AWS_Simple_Notification_Service/variables.tf
+++ AWS_Simple_Notification_Service/variables.tf
@@ -16,3 +16,47 @@ variable "profile" {
 ################################################################################
 # SNS  Variables
 ################################################################################
+variable "sns_topic_name" {
+  type    = string
+  default = "TestSNSTopic.fifo"
+}
+
+variable "kms_key_id" {
+  type    = string
+  default = "eaa3ed72-51a7-4d4f-a5a5-dcb8dd387804"
+}
+
+variable "minDelayTarget" {
+  type    = number
+  default = 20
+}
+
+variable "maxDelayTarget" {
+  type    = number
+  default = 20
+}
+
+variable "numRetries" {
+  type    = number
+  default = 3
+}
+
+variable "numMaxDelayRetries" {
+  type    = number
+  default = 0
+}
+
+variable "numNoDelayRetries" {
+  type    = number
+  default = 0
+}
+
+variable "numMinDelayRetries" {
+  type    = number
+  default = 0
+}
+
+variable "backoffFunction" {
+  type    = string
+  default = "linear"
+}
