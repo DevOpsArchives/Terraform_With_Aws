a3abd957b62c12201f1061d59042b70cdbf98918
Subhadeep Banerjee <subhadeep762@gmail.com>
2022-09-18 21:31:09 +0530

    Add variables for SQS resource

--- AWS_Simple_Queue_Service/variables.tf
+++ AWS_Simple_Queue_Service/variables.tf
@@ -16,3 +16,71 @@ variable "profile" {
 ################################################################################
 # SQS Variables
 ################################################################################
+
+variable "queue_name" {
+  type=string
+  default = "TestQueue.fifo"
+}
+
+variable "is_fifo" {
+  type=bool
+  default = true
+}
+
+variable "deduplication_scope" {
+  type = string
+  default = "messageGroup"
+}
+
+variable "fifo_throughput_limit" {
+  type = string
+  default = "perMessageGroupId"
+}
+
+variable "content_based_deduplication" {
+  type = bool
+  default = true
+}
+variable "max_msg_size" {
+  type = number
+  default = 131072
+}
+
+variable "delay_seconds" {
+  type = number
+  default = 0
+}
+
+variable "visibility_timeout_seconds" {
+  type = number
+  default = 300
+}
+
+variable "msg_retention_seconds" {
+  type = number
+  default = 604800
+}
+
+variable "recv_wait_time_seconds" {
+  type = number
+  default = 5
+}
+
+variable "max_recieve_count" {
+  type = number
+  default = null
+}
+
+variable "is_encryption_enabled" {
+  type = bool
+  default=true
+}
+variable "kms_key_id" {
+  type = string
+  default = "5163017c-e55f-4e0e-8b4f-0c74c7a60319"
+}
+
+variable "kms_data_reuse_time" {
+ type=number
+ default = 300 
+}
