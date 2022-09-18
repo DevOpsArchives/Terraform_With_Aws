75db2686dd48c03f06242003996607352afb38a1
Subhadeep Banerjee <subhadeep762@gmail.com>
2022-09-18 13:36:43 +0530

    Add kms key vars to variables.tf

--- AWS_Key_Management_Service/variables.tf
+++ AWS_Key_Management_Service/variables.tf
@@ -12,3 +12,47 @@ variable "profile" {
   default     = "self"
   description = "AWS Profile"
 }
+
+################################################################################
+# KMS  Variables
+################################################################################
+
+variable "key_desc" {
+  type    = string
+  default = "Customer Managed Key"
+}
+
+variable "key_usage" {
+  type    = string
+  default = "ENCRYPT_DECRYPT"
+}
+
+variable "master_key_spec" {
+  type    = string
+  default = "SYMMETRIC_DEFAULT"
+}
+
+variable "deletion_window" {
+  type    = number
+  default = 7
+}
+
+variable "key_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "key_rotation" {
+  type    = bool
+  default = true
+}
+
+variable "multi_region" {
+  type    = bool
+  default = false
+}
+
+variable "key_tag" {
+  type    = string
+  default = "TestKey"
+}
