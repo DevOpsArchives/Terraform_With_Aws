86896e6e0450cb832fc3490af5e8615b11c24aef
Subhadeep Banerjee <subhadeep762@gmail.com>
2022-09-18 19:43:29 +0530

    Initial setup for SQS

---
+++ AWS_Simple_Queue_Service/variables.tf
@@ -0,0 +1,18 @@
+################################################################################
+# General Variables
+################################################################################
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "Region where our infrastructure is deployed"
+}
+
+variable "profile" {
+  type        = string
+  default     = "self"
+  description = "AWS Profile"
+}
+
+################################################################################
+# SQS Variables
+################################################################################
