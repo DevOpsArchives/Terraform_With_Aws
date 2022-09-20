db793411f8942219415731916092b535c92a5128
Subhadeep Banerjee <subhadeep762@gmail.com>
2022-09-20 15:22:27 +0530

    Add vars for vault and plan resources

--- AWS_Backup/variables.tf
+++ AWS_Backup/variables.tf
@@ -16,3 +16,37 @@ variable "profile" {
 ################################################################################
 # Backup  Variables
 ################################################################################
+variable "vault_name" {
+  type    = string
+  default = "TestVault"
+}
+
+variable "rule_name" {
+  type    = string
+  default = "TestBackupRule"
+}
+
+variable "backup_plan_name" {
+  type    = string
+  default = "TestBackupPlan"
+}
+
+variable "schedule_exp" {
+  type    = string
+  default = "cron(0 12 * * ? *)"
+}
+
+variable "continuous_backup" {
+  type    = bool
+  default = true
+}
+
+variable "start_window" {
+  type    = number
+  default = 480
+}
+
+variable "completion_window" {
+  type    = number
+  default = 2880
+}
