ab55539a081e2915c9e327243b97da5e652f92db
Subhadeep Banerjee <subhadeep762@gmail.com>
2022-09-26 15:45:08 +0530

    Add vars fr basic vpc and vpc with IPAM

--- AWS_CloudWatch/variables.tf
+++ AWS_CloudWatch/variables.tf
@@ -16,3 +16,20 @@ variable "profile" {
 ################################################################################
 # VPC  Variables
 ################################################################################
+variable "vpc_name" {
+  type = string
+  default = "TestVPC"
+  description = "Name of the VPC to be used"
+}
+
+variable "cird_range" {
+  type = string
+  default = "10.0.0.0/24"
+  description = "CIRD range for the VPC"
+}
+
+variable "vpc_mode" {
+  type = string
+  default = "vpc_baisc"
+  description = "The Type of vpc we want to use vpc_baisc/vpc_ipam"
+}
